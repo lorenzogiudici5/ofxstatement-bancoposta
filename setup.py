e67eb20d2e5d1f3db4f361d604b1e4dc50c51cad
Lorenzo Giudici <lorenzogiudici5@gmail.com>
2023-10-29 16:12:25 +0100

    Add Pdf and Csv Bancoposta parser (#1)
    Add BancopostaCsvParser and BancopostaPdfParser in order to parse pdf and csv statement in ofx file

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """Setup
 """
+import os
 from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
 from distutils.core import setup
 
-version = "0.0.1"
+import unittest
 
-with open("README.rst") as f:
-    long_description = f.read()
+version = "1.0.0"
 
 setup(
-    name="ofxstatement-sample",
+    name="ofxstatement-bancoposta",
     version=version,
-    author="Andrey Lebedev",
-    author_email="andrey@lebedev.lt",
-    url="https://github.com/kedder/ofxstatement",
-    description=("Sample plugin for ofxstatement"),
-    long_description=long_description,
+    author="Lorenzo Giudici",
+    author_email="lorenzogiudici5@gmail.com",
+    url="https://github.com/lorenzogiudici5/ofxstatement-bancoposta",
+    description=("Bancoposta plugin for ofxstatement"),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     license="GPLv3",
-    keywords=["ofx", "banking", "statement"],
+    keywords=["ofx", "banking", "statement", "bancoposta"],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Natural Language :: English",
         "Topic :: Office/Business :: Financial :: Accounting",
@@ -33,9 +35,13 @@ setup(
     package_dir={"": "src"},
     namespace_packages=["ofxstatement", "ofxstatement.plugins"],
     entry_points={
-        "ofxstatement": ["sample = ofxstatement.plugins.sample:SamplePlugin"]
+        "ofxstatement":
+        [
+            "bancoposta = ofxstatement.plugins.bancoposta:BancoPostaPlugin"
+        ]
     },
     install_requires=["ofxstatement"],
+    extras_require={"test": ["pytest"]},
     include_package_data=True,
     zip_safe=True,
 )
